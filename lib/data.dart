769913ac150221a78f9a7a3dc35694e6b70bc5a5
marcdc-tm <marc.decaluwe@thomasmore.be>
2022-03-06 20:47:31 +0100

    init

---
+++ lib/data.dart
@@ -0,0 +1,132 @@
+library lyrics;
+
+/*
+List<Lyric> lyrics = [
+  Lyric(
+      title: '8 Circle',
+      artist: 'Bon Iver',
+      type: Genre.folktronica,
+      imageName: '22.jpg',
+      text: 'Philosophize your figure
+What I have and haven't held
+You called and I came, stayed tall through it all
+Fall and fixture just the same thing
+Say nothing of my fable, no
+What on earth is left to come
+Who's agonized and gnawed through it all
+I'm underneath your tongue
+I'm standing in your street now, no
+And I carry his guitar
+And I can't recall it lightly at all
+But I know I'm going in
+Too much for me to pick up, no
+Not sure what forgiveness is
+We've galvanized the squall of it all
+I can leave behind the harbor
+I will run (all around it)
+Have to crawl (still can't stop it)
+Along the fires
+One more time just pass me by
+I'ma make it half the night
+Hall light wishes
+To walk aside your favor
+I'm an Astuary King
+I'll keep in a cave, your comfort and all
+Unburdened and becoming
+I will run (all around it)
+(Sailing off and on, you're on your honor)
+Have to crawl
+(Carry off and up your seize) still can't
+I will run (carry up love along unfettered time)
+Now Mona, now Mona
+Haven't I locked up my failure (sailing off and ore your odd, your honor)
+From the core to the sky
+Wouldn't I be last to see? (Carry off and up your seize)
+From the door 'til we die
+Deny with love my labor (carry up with love a long)
+Was it all I could find (we live outside)
+Keep waking up high'
+    ),
+  Lyric(
+    title: 'I won't run from it',
+    artist: 'Big Red Machine',
+    type: Genre.electronic,
+    imageName: 'bigred.jpg',
+    text: 'Now, look at that road
+Look at that climb
+When it's throwing in a nickel or get out a dime
+Too patterned, too close
+Too flattered, too broke
+Now look at you go
+Took the morning to float
+Took the morning to find
+I got a vision of a mission but an awkward vibe
+Too livid, too scared
+Too loving, too closed
+Now look at you know
+When the cause is on dead arrival
+And you coulda shoulda woulda for free
+Now look at your post script, every minute
+It's a very slow thing to have glean, ah
+So maybe you know
+Or maybe you don't
+It isn't time for a fitting of a passing cloak
+Too forward, too fine
+Too patient, too wise
+Now look at you shine
+Took the matter to house
+Too the matter to mouth
+With the feeling that I'm fishing in a harbor now
+Too clever, too kind
+Too patterned to hide
+Now, look at that smile
+So when the 'cause ain't dead on arrival
+And you couldn't shouldn't wouldn't for free
+Do not hang your cause on revival
+'Cause now looking is bringing you grief
+So when the cause is dead on arrival
+And you coulda shoulda woulda for free
+I wouldn't have forced it on the minute
+It's a very hard thing to have grief
+Ah give it a minute, we're dancing in it',
+  ),
+  Lyric(
+    title: 'Nothing Arrived',
+    artist: 'Villagers',
+    type: Genre.indiefolk,
+    imageName: 'awayland.jpg',
+    text: 'Savanna scatters and the seabird sings
+So why should we fear what travel brings?
+What were we hoping to get out of this?
+Some kind of momentary bliss?
+I waited for something and something died
+So I waited for nothing and nothing arrived
+It's our dearest ally, it's our closest friend
+It's our darkest blackout, it's our final end
+My dear sweet nothing, let's start a new
+From here all in is just me and you
+I waited for something
+And something died
+So I waited for nothing
+And nothing arrived
+Well I guess it's over
+I guess it's begun
+It's a losers' table, but we've already won
+It's a funny battle, it's a constant game
+I guess I was busy when nothing came
+I guess I was busy (when nothing arrived)
+I guess I was busy (when nothing arrived)
+I guess I was busy (when nothing arrived)
+I guess I was busy (when nothing arrived)
+I waited for something
+And something died
+So I waited for nothing
+And nothing arrived
+I waited for something
+And something died
+So I waited for nothing
+And nothing arrived
+(Nothing arrived)',
+  )
+]
+*/
